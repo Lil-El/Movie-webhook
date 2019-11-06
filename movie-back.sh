4eb9303b5b7e3d89ad6d9a4866e25dc94ecb5ec4
Lil-El <yxd99324@qq.com>
2019-11-06 19:57:11 +0800

    gen sh script

---
+++ movie-back.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+WORK_PATH='/home/project/Movie-back'
+cd WORK_PATH
+echo '清除老代码'
+git reset --hard origin/master
+gir clean -f
+echo '拉取新代码'
+git pull origin master
+echo '开始构建'
+docker build -t movie-back:1.0.0 .
+echo '删除旧容器'
+docker container stop movie-back-container
+docker rm movie-back-container
+echo '启动容器'
+docker container run -p 3000:3000 --name movie-back-container -d movie-back:1.0.0
+
